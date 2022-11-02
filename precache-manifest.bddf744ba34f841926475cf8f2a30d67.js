28044ff121d0c61d957aefc0d70df263af0e5d02
Alsaxian <kurt.goedel.1930@googlemail.com>
2022-11-02 15:57:29 +0000

    Deploying to gh-pages from @ Alsaxian/gitactions_demo@0ddfe8c1e7f95ee6e9b33df2e2e976b2274e4fa9 🚀

---
+++ precache-manifest.bddf744ba34f841926475cf8f2a30d67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283749aa57625d5b0ebc9de3084317bc",
+    "url": "/gitactions_demo/index.html"
+  },
+  {
+    "revision": "c5e1d63dd37b8f518042",
+    "url": "/gitactions_demo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "55c935a2be73a9c55687",
+    "url": "/gitactions_demo/static/js/2.d0dec738.chunk.js"
+  },
+  {
+    "revision": "c5e1d63dd37b8f518042",
+    "url": "/gitactions_demo/static/js/main.54767995.chunk.js"
+  },
+  {
+    "revision": "7db879091eff031ab835",
+    "url": "/gitactions_demo/static/js/runtime~main.9f27ac19.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gitactions_demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github-actions-demo/precache-manifest.a17dcf3eb84416f02db7e69aa4bc54bd.js"
+  "/gitactions_demo/precache-manifest.bddf744ba34f841926475cf8f2a30d67.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-actions-demo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gitactions_demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
